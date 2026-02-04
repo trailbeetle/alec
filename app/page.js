0afb6cddd6e0483a7c87165339f7c4f1b631558c
Laura Trebe <lauratrebe@Lauras-MacBook-Air.local>
2026-02-03 20:35:24 -0600

    update page.js

--- app/page.js
+++ app/page.js
@@ -7,32 +7,22 @@ const sayings = [
   "the way you start to randomly massage my arms or neck when we are just sitting around doing nothing otherwise",
 "That you remember so many little details of me. (“…but you don’t like people in your art, that’s why you don’t like it” example)",
 "Your face, your whole face, including your eyes and their brows",
-  "your perfect blend of challenging me while still validating and soothing me if im having trouble with an area of my life",
-  "your constant confidence in me and my abilities",
-    "the way you always have something nice to say about my cooking no matter how many times i do it for you",
-  "your gentle softness, mentally and physically  ",
-  "the way you work a hard hard job and rarely complain about it, even when youre tired",
-  "when you beat me in tekken and perfectly mix being nice and taunting about it",
-  "you for introducing me to new music",
-  "the way you make it easy for me to imagine a future with you",
-  "that you follow news and politics and have such strong opinions on what is right and wrong within them",
-  "your interest in self growth",
-    "the way i feel great after seeing you, even if it was only for a little while",
-  "the way you dance so joyously and freely",
-  "the way you make me feel free and unencumbered",
-  "the way your hair curls after a shower",
-  "the way your shoulders look when youre reaching up for something or laying down (and all the rest of the time)",
-  "the way your voice softens when you tell me something sweet",
-   "the way you kiss me 50x whenever you get on a roll",
-  "your beautiful doe eyes",
-     "your music taste",
-  "your attentiveness to my nuances and genuine interest in figuring them out",
-  "the way you take care of your skin and your body, because you are embodied and care about how you feel",
-  "the way you keep me on my toes (always one upping me in jokes and i love it)",
-  "the silky softness of your hair, especially the part that lays over your forehead",
-  "the way you do little things to let me know you're thinking of me, like writing me a little note or bringing me an energy drink",
-  "the way you can take any joke i think we've hit the punchline on and come up with an even better one",
-  "your unyielding honesty with me and yourself",
+"when we tangle our legs while going to bed",
+"who I am becoming as a person through being supported by you",
+"the perfectly curated memes you send me",
+"keeping you on your toes",
+"that i feel totally comfortable and not self conscious around you",
+"making you laugh and trying to get better at doing it",
+"imagining living with you",
+"being inspired by you to engage in art and better myself in general",
+"taking care of you",
+"so much that i hope i get to spend the rest of my life with you. I love who I am with you, and every aspect of your being. You are a beautiful soul and I am grateful every day that we choose to love each other. I deeply hope that our values continue to align and that I will one day get to call you my husband, for real. ",
+"feeling like you are more than enough to satisfy my needs - my friendships ending werent even a blip because you support me (but in a healthy way)",
+"the way we always bring little gifts back for each other when we go out (vacations, the grocery store, etc)",
+"going out with you and being seen with you by everybody",
+"the delicious meals we come up with together and share",
+"buying or making you things you want or need",
+"when you bring me flowers (HINT)"
 ]
 
 
@@ -75,7 +65,9 @@ console.log("You found this? good job techie babe :) my fave curious person ever
     <div style={ backgroundStyle }>
     <div style={sectionStyle }>
       <h1 style={{textDecoration: 'underline', fontWeight: 'bold'}}>I love</h1>
-      <div>{saying}</div>
+      {(date.getDate() === 14) ? <h2 style={{color: 'purple'}}>{saying}</h2> :  <div>{saying}</div>
+      }
+     
     </div>
     </div>
   );
